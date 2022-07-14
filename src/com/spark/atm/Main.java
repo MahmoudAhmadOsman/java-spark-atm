842e24834baafbd7d733addb7d73b5cfff09fe82
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-07-13 19:28:39 -0500

    added deposit and withdrawAmount implementations

--- src/com/spark/atm/Main.java
+++ src/com/spark/atm/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-   public static final String RED_BACKGROUND = "\u001B[41m";
+    public static final String RED_BACKGROUND = "\u001B[41m";
 
     public static void main(String[] args) {
 //        int atnNum = 25000;
@@ -15,7 +15,7 @@ public class Main {
 
 
         double accountNumber = Math.random() * 25000;
-        double pinNumber     = Math.random() * 5000;
+        double pinNumber = Math.random() * 5000;
 
         // cast the double to whole number
         int atnNum = (int) accountNumber;
